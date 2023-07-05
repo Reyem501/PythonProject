c7aa7234f9d23107bffac632a747d5a29914c12e
Reyem501 <59642674+Reyem501@users.noreply.github.com>
2023-07-05 16:25:35 +0200

    models

--- miniprojet/lycee/models.py
+++ miniprojet/lycee/models.py
@@ -3,28 +3,32 @@ from django.db import models
 
 # Create your models here.
 
-class Enseignant(models.Model):
-    first_name = models.CharField(
+class Emprunt(models.Model):
+    budget = models.CharField(
         max_length=50,
         blank=False,
         null=False
     )
-    last_name = models.CharField(
+    def __str__(self):
+        return self.nom
+
+
+class Enseignant(models.Model):
+    prenom = models.CharField(
+        max_length=50,
+        blank=False,
+        null=False
+    )
+    nom = models.CharField(
         verbose_name="lastname",
         help_text="last name of the enseignant",
         blank=False,  # pas de champ vide
         null=False,  # pas de champ null (a conjuguer avec default
         default="???",
-        max_length=255,  # taille maximale du champ
+        max_length=50,  # taille maximale du champ
     )
-    cursus = models.ForeignKey(
-        Cursus,
-        on_delete=models.CASCADE,  # necessaire selon la version de Django
-        null=True
-    )
-
     def __str__(self):
-        return self.last_name
+        return self.nom
 
 
 class Materiel(models.Model):
@@ -33,7 +37,40 @@ class Materiel(models.Model):
         blank=False,
         null=False
     )
-    quantite_materiel = models.IntegerField(blank=False, null=False)  # Ajout du champ "quantite"
-
+    localisation = models.CharField(
+        max_length=50,
+        blank=False,
+        null=False
+    )
+    responsable = models.CharField(
+        max_length=50,
+        blank=False,
+        null=False
+    )
+    enseignant = models.ForeignKey(
+        Enseignant,
+        on_delete=models.CASCADE,  # necessaire selon la version de Django
+        null=True
+    )
     def __str__(self):
-        return self.last_name
+        return self.libelle_materiel
+
+
+class Accessoire(models.Model):
+    libelle_acc = models.CharField(
+        max_length=50,
+        blank=False,
+        null=False
+    )
+    etat = models.CharField(
+        max_length=50,
+        blank=False,
+        null=False
+    )
+    materiel = models.ForeignKey(
+        Materiel,
+        on_delete=models.CASCADE,  # necessaire selon la version de Django
+        null=True
+    )
+    def __str__(self):
+        return self.libelle_acc
